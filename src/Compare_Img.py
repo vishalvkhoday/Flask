eb696aa42fd42992d799d1ff511519c72a1615a1
vkhoday <vkhoday@192.168.56.1>
2019-03-26 15:48:18 +0530

    included extra spaces to make image better

--- src/Compare_Img.py
+++ src/Compare_Img.py
@@ -9,8 +9,10 @@ from selenium import webdriver
 import datetime
 import os
 import sys
+import HtmlTestRunner
+import unittest
 
-class ScreenAnalysis:
+class ScreenAnalysis(unittest.TestCase):
 
     STAGING_URL = 'https://www.google.com/search?q=NYSE%3A+DXC&oq=NYSE%3A+DXC&aqs=chrome..69i57j69i58.4562j0j7&sourceid=chrome&ie=UTF-8'
     PRODUCTION_URL = 'https://www.google.com/search?q=NYSE%3A+DXC&oq=NYSE%3A+DXC&aqs=chrome..69i57j69i58.4562j0j7&sourceid=chrome&ie=UTF-8'
@@ -19,19 +21,22 @@ class ScreenAnalysis:
     
     def __init__(self):
         self.set_up()
-        self.capture_screens()
+        self.Test_capture_screens()
         self.analyze()
         self.clean_up()
 
     def set_up(self):
-        self.driver = webdriver.Chrome()
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("start-maximized")
+        self.options.add_argument("disable-infobars")
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+#         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
     def clean_up(self):
         self.driver.close()
 
-    def capture_screens(self):
-        
+    def Test_capture_screens(self):        
         self.screenshot(self.STAGING_URL, 'screen_Base.png')
         self.screenshot(self.PRODUCTION_URL, 'screen_NewCode.png')
 
@@ -39,14 +44,12 @@ class ScreenAnalysis:
         print ("Capturing", url, "screenshot as", file_name, "...")
         self.driver.get(url)
 #         xpath_logo= '//*[@id="automatedSlider"]'
-        if file_name=='screen_Base.png':
-            
+        if file_name=='screen_Base.png':            
             elem_scr =self.driver.find_element_by_class_name("VADlJf")
             self.Elem_ScreenShot(elem_scr,file_name)
             
             
-        else:
-            
+        else:            
             elem_scr =self.driver.find_element_by_class_name("VADlJf")
             self.Elem_ScreenShot(elem_scr,file_name)
 #         self.driver.get_screenshot_as_png()
@@ -56,16 +59,14 @@ class ScreenAnalysis:
         location=element.location
         size =element.size
         png = self.driver.get_screenshot_as_png()
+        with Image.open(BytesIO(png)) as im:
+            left=(location['x']- location['x']//20 )- location['x']// 20
+            top=location['y'] - location['y']//5
+            right =(location['x']-location['x']//20)+(size['width'] + size['width']//20)
+            bottom =(location['y']-location['y']//20)  +(size['height'] + size['height']//5)
+            im=im.crop((left,top,right,bottom))
+            im.save('screenshots/{}'.format(file_name))
         
-        im= Image.open(BytesIO(png))
-        left=location['x']
-        top=location['y']
-        right =location['x']+size['width']
-        bottom =location['y']+size['height']
-        im=im.crop((left,top,right,bottom))
-        im.save('screenshots/{}'.format(file_name))
-        
-    
     def analyze(self):
         print("Image Analysis started !!!")
         screenshot_staging = Image.open("screenshots/screen_Base.png")
@@ -85,6 +86,9 @@ class ScreenAnalysis:
                     draw = ImageDraw.Draw(screenshot_staging)
 #                     draw = ImageDraw.Draw(screenshot_production)
                     
+#                     draw.rectangle((x, y, x+block_width, y+block_height), outline = "red")
+#                     draw.line((x, x+block_width), fill=256,width=block_width)
+#                     draw.line((x, x+block_width), fill=128,width=block_width)
                     draw.rectangle((x, y, x+block_width, y+block_height), outline = "red")
                     Res="Fail"
         TM =datetime.datetime.now()
@@ -109,5 +113,8 @@ class ScreenAnalysis:
 
         return region_total/factor
 
-ScreenAnalysis()
+if __name__ == "__main__":
+    ScreenAnalysis()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/vkhoday/eclipse-workspace/Pyunit_Selenium/Reports'))
+    
 
--- src/Element_Screen_shot.py
+++ src/Element_Screen_shot.py
@@ -10,7 +10,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 options.binary_location = "/usr/bin/chromium"
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options,executable_path='C:/Users/vkhoday/git/Selenium_NSE_Algo/Additonal_Utility/chromedriver_235')
  
 driver.get('https://python.org')
 driver.save_screenshot("screenshot.png")
